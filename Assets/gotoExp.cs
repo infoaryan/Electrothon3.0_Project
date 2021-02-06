f7dcf71f36a5d9ed88f6904f4b8892a8b5cea980
Aryan Verma <aryanverma19oct@gmail.com>
2021-02-06 14:23:15 +0530

    Added project

---
+++ Assets/gotoExp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class gotoExp : MonoBehaviour {
+
+	public void loadohmslawscene(){
+		SceneManager.LoadScene("ohmsLaw");
+	}
+	public void loadfullrectifierscene(){
+		SceneManager.LoadScene("fullRectifier");
+	}
+}
---
+++ Assets/iunderstand.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class iunderstand : MonoBehaviour {
+
+	public void loadthirdscene(){
+		SceneManager.LoadScene("experiments");
+	}
+}
